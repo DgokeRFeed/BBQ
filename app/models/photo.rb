87a37f094f1b759633d68ebe09dad31dea32881e
DgokeRFeed <popovvova99@yandex.ru>
2022-09-05 21:15:22 +0300

    small fix with photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -9,6 +9,7 @@ class Photo < ApplicationRecord
 
   validates :event, presence: true
   validates :user, presence: true
+  validates :photo, presence: true
 
   scope :persisted, -> { where "id IS NOT NULL" }
 end
